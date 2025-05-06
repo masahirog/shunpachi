a18869816f61c14d6df2cda42f81c4690d0e58d7
masahirog <masahiro11g@gmail.com>
2025-05-06 10:07:52 +0900

    no message

--- db/migrate/20250505045121_create_raw_materials.rb
+++ db/migrate/20250505045121_create_raw_materials.rb
@@ -1,23 +1,13 @@
 class CreateRawMaterials < ActiveRecord::Migration[7.1]
-  # def change
-  #   create_table :raw_materials do |t|
-  #     t.string :name
-  #     t.integer :category
-  #     t.text :description
+  def change
+    create_table :raw_materials do |t|
+      t.string :name
+      t.integer :category
+      t.text :description
 
-  #     t.timestamps
-  #   end
+      t.timestamps
+    end
 
-  #   add_index :raw_materials, :name, unique: true
-  # end
-  def down
-    
-    # 外部キー制約を一時的に無効にする
-    execute "SET FOREIGN_KEY_CHECKS=0;"
-    drop_table :raw_materials
-    execute "SET FOREIGN_KEY_CHECKS=1;"
-    
-    # または単純にコメントアウトする場合
-    # # drop_table :raw_materials
+    add_index :raw_materials, :name, unique: true
   end
 end
